bf34146efb68875d4da45d61701e8129fc234c2a
EnderAvaritia <enderavaritia@gmail.com>
2024-06-25 21:30:36 +0800

    可以发送网页，被群友糊弄了

--- server/simple_http_server.py
+++ server/simple_http_server.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import json
 import os
 
+server_host='127.0.0.1'
 server_port=8080
 last_data={"error": "no_data"}
 
@@ -15,17 +16,27 @@ def init_record_file():
                 #初始化记录文件
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def _set_response(self):
+    def _set_response(self,content_type):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        if content_type=="json":
+            self.send_header('Content-type', 'application/json')
+        elif content_type=="html":
+            self.send_header('Content-type', 'text/html')
+        elif content_type=="text":
+            self.send_header('Content-type', 'text/plain')
+        else:
+            self.send_header('')
         self.end_headers()
-        #通用的响应头
+        #响应头
 
     def do_GET(self):
-        global last_data
-        self._set_response()
-        self.wfile.write(json.dumps(last_data).encode('utf-8'))
+        self._set_response("html")
+        #global last_data
+        #self.wfile.write(json.dumps(last_data).encode('utf-8'))
         #遗憾的是socketserver不能返回网页，get方法当作api用吧
+        with open("./index.html",'r',encoding='utf-8') as index:
+            self.wfile.write(index.read().encode('utf-8'))
+        #艹，被群友糊弄了，不能个头子
         
     def do_POST(self):
         global last_data
@@ -35,12 +46,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         #print(str(post_data))
         content_type=str(self.headers['Content-Type'])
         #print(content_type)
-        self._set_response()
         #以上是post方法通用的部分
         
         if content_type=="application/json":
             #print("json")
             last_data = json.loads(post_data)
+            self._set_response("json")
             self.wfile.write(json.dumps({"received": last_data}).encode('utf-8'))
             #这里要不要发个时间回去
             #这里是给采集器发送采集获得的数据用的
@@ -56,15 +67,21 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             data=post_data.decode('utf-8')
             if str(data)=="time":
                 current_date_and_time = str(datetime.now().strftime("%Y/%D-%H:%M:%S"))
-                self.wfile.write(json.dumps({"datetime": current_date_and_time}).encode('utf-8'))
-                #同步时间用的，采集器需要发送一个为text类型的内容为time的post请求
+                
+                #self._set_response("json")
+                #self.wfile.write(json.dumps({"datetime": current_date_and_time}).encode('utf-8'))
+                
+                self._set_response("text")
+                self.wfile.write(current_date_and_time.encode('utf-8'))
+                
+                #同步时间用的，采集器需要发送一个为text类型的内容为time的post请求,响应体用text还是json自己选
                 
         else:
             self.wfile.write("unexpended_request".encode('utf-8'))
             #万一采集器发了点什么奇奇怪怪的东西过来
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=server_port):
-    server_address = ('', port)
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler,host=server_host , port=server_port):
+    server_address = (host, port)
     with server_class(server_address, handler_class) as httpd:
         print(f'Starting httpd on port {port}...')
         httpd.serve_forever()
